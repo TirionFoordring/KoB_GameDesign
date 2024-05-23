3e19f38ba2375387c4d69759ae0963f5519f1f6c
TirionFoordring <sxqt316623@163.com>
2024-05-24 00:26:34 +0900

    Jwt security update 2024/05/24 00:26

--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -10,10 +10,6 @@ class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
-		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		System.out.println(passwordEncoder.encode("jin"));
-		System.out.println(passwordEncoder.encode("ljy"));
-		System.out.println(passwordEncoder.encode("pdh"));
 
 	}
 
