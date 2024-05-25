eff5af2a73326c1a325d4c9dbc41d93f95229714
TirionFoordring <sxqt316623@163.com>
2024-05-25 23:05:31 +0900

    update backend: implement 3 interfaces login, getInfo and register

--- web/src/App.vue
+++ web/src/App.vue
@@ -27,7 +27,38 @@ export default {
       error(resp) {
         console.log(resp);
       }
-    })
+    });
+
+    $.ajax({
+      url: "http://localhost:3000/user/account/info/",
+      type: "get",
+      headers: {
+        // "Bearer "在config.filter.JwtAuthenticationTokenFilter.java这个文件中定义
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxZWM0ZmVhODJlZWU0YzAyODE1ZTEyNDZhMWFkMjU2YiIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTcxNjY0MDg3NCwiZXhwIjoxNzE3ODUwNDc0fQ.aCvtOU2Bo-L8lldm84iNWsz8h2bTz8gUVLIRl0e3szs",
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    });
+
+    // $.ajax({
+    //   url: "http://localhost:3000/user/account/register/",
+    //   type: "post",
+    //   data: {
+    //     username: "jinshaobo",
+    //     password: "123456",
+    //     confirmedPassword: "123456",
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 
