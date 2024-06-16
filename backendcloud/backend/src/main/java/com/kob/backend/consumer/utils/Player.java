52aa34378a5d63a324af43a15fe2616f773a819b
TirionFoordring <sxqt316623@163.com>
2024-06-16 20:51:02 +0900

    Microservices: Matching System

---
+++ backendcloud/backend/src/main/java/com/kob/backend/consumer/utils/Player.java
@@ -0,0 +1,55 @@
+package com.kob.backend.consumer.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+    private Integer id;
+    private Integer sx;
+    private Integer sy;
+    private List<Integer> steps;
+
+    //检查蛇是否会变长
+    private boolean checkTailIncrease(int step){
+        if (step <= 10) return true;
+        return step % 3 == 1;
+    }
+
+    public List<Cell> getCells(){
+        List<Cell> res = new ArrayList<>();
+
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        int x = this.sx;
+        int y = this.sy;
+        int step = 0;
+
+        res.add(new Cell(x,y));
+        for (int d : steps){
+            x += dx[d];
+            y += dy[d];
+            res.add(new Cell(x,y));
+            step ++;
+            if(!checkTailIncrease(++ step)){
+                res.remove(0);
+            }
+        }
+        return res;
+    }
+
+    public String getStepsString(){
+        StringBuilder resp = new StringBuilder();
+        for (Integer s : steps){
+            resp.append(s);
+        }
+        return resp.toString();
+    }
+}
---
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/service/impl/utils/Player.java
@@ -0,0 +1,14 @@
+package com.kob.matchingsystem.service.impl.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+    private Integer userId;
+    private Integer ranking;
+    private Integer waitingTime; // 匹配时的等待时间
+}
