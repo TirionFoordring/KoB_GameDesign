ff51b8d8033f7874b70de26c82eebad33890f522
TirionFoordring <sxqt316623@163.com>
2024-07-23 23:24:18 +0900

    update backend: Record List

--- web/src/views/record/RecordIndex.vue
+++ web/src/views/record/RecordIndex.vue
@@ -4,11 +4,39 @@
 
 <script>
 import ContentField from "../../components/ContentField.vue"
+import { useStore } from "vuex";
+import $ from "jquery";
 
 export default {
-    components: {
-        ContentField
+  components: {
+    ContentField
+  },
+  setup() {
+    const store = useStore();
+    let current_page = 1;
+
+    const pull_page = page => {
+      current_page = page;
+      $.ajax({
+        url: "http://localhost:3000/record/getlist/",
+        data: {
+          page,
+        },
+        type: "get",
+        headers: {
+          Authorization: "Bearer " + store.state.user.token,
+        },
+        success(resp){
+          console.log(resp);
+        },
+        error(resp) {
+          console.log(resp);
+        },
+      })
     }
+
+    pull_page(current_page);
+  }
 }
 </script>
 
